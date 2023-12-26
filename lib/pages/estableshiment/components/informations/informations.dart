a1340405220c7d7f998132eae711ac5b72f231f4
iCarlosAugusto <augusto.dev@proton.me>
2023-12-26 15:28:23 -0300

    feat: :sparkles: Adicionando mais coisas as informaçoes

--- lib/pages/estableshiment/components/informations/informations.dart
+++ lib/pages/estableshiment/components/informations/informations.dart
@@ -1,4 +1,4 @@
-import 'package:cabelin_v2/widgets/list_widget.dart';
+import 'package:cabelin_v2/widgets/button_widget.dart';
 import 'package:cabelin_v2/widgets/text_widget.dart';
 import 'package:flutter/material.dart';
 
@@ -7,20 +7,50 @@ class Informations extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        const TextWidget("(62) 98239-8342", margin: EdgeInsets.only(bottom: 16, top: 16)),
-        const TextWidget("Horários", customFontsize: 22),
-        ListView.builder(
-          padding: EdgeInsets.zero,
-          shrinkWrap: true,
-          physics: const NeverScrollableScrollPhysics(),
-          itemBuilder: (_, __) => const TextWidget("Segunda 8:00 - 16:00", customFontsize: 16),
-          itemCount: 10
-        ),
+    return SingleChildScrollView(
+      primary: false,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          const TextWidget("Barbearia com mais 10 de anos de tradição sempre pensando no bem estar do cliente"),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              const TextWidget(
+                "(62) 98239-8342",
+                margin: EdgeInsets.only(bottom: 16, top: 16)
+              ),
+              ButtonWidget(
+                title: "Ligar",
+                onTap: () {
 
-      ],
+                }
+              )
+            ],
+          ),
+          const Divider(),
+          const TextWidget(
+            "Horários",
+            customFontsize: 22,
+            customWeight: FontWeight.w800,
+          ),
+          ListView.builder(
+            padding: EdgeInsets.zero,
+            shrinkWrap: true,
+            primary: false,
+            physics: const NeverScrollableScrollPhysics(),
+            itemBuilder: (_, __) => const Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                TextWidget("Segunda-feira"),
+                TextWidget("8:00 - 16:00", customWeight: FontWeight.w800)
+              ],
+            ),
+            itemCount: 10
+          ),
+      
+        ],
+      ),
     );
   }
 }
