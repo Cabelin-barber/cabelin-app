73889dc48e6e726f23f12ec317c4e4802633d444
iCarlosAugusto <augusto.dev@proton.me>
2023-12-27 12:10:58 -0300

    feat: :sparkles: login social config

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,15 @@
+import 'package:cabelin_v2/firebase_options.dart';
 import 'package:cabelin_v2/routes/main.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform 
+  );
+
   runApp(const MyApp());
 }
 
