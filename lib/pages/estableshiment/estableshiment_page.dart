42ab2073c870cbdf06c664ff4ea5f3eaab389e06
iCarlosAugusto <augusto.dev@proton.me>
2023-12-30 14:43:26 -0300

    refactor: :recycle: Melhorado a tela do estabelecimento

--- lib/pages/estableshiment/estableshiment_page.dart
+++ lib/pages/estableshiment/estableshiment_page.dart
@@ -1,7 +1,6 @@
 import 'package:cabelin_v2/pages/estableshiment/components/informations/informations.dart';
 import 'package:cabelin_v2/pages/estableshiment/components/portfolio/portfolio.dart';
 import 'package:cabelin_v2/pages/estableshiment/components/services_offer/services_offer.dart';
-import 'package:cabelin_v2/widgets/text_widget.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 
@@ -10,107 +9,64 @@ class EstlableshimentPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: DefaultTabController(
-        length: 3,
-        child: NestedScrollView(
-          headerSliverBuilder: (BuildContext context, bool innerBoxIsScrolled) {
-            return <Widget>[
-              SliverAppBar(
-                expandedHeight: 200.0,
-                floating: false,
-                pinned: true,
-                leading: IconButton(
-                  onPressed: () {
-                    context.pop();
-                  },
-                  icon: const Icon(
-                    Icons.chevron_left_rounded,
-                    size: 52,
-                    color: Colors.white,
-                  )
-                ),
-                flexibleSpace: FlexibleSpaceBar(
-                  background: Image.network(
-                    "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.5323gYZjoqFwr0zrN28KPAHaE8%26pid%3DApi&f=1&ipt=96d6cb3c800087f979dcfed334aaa919f198619cfcf66cec8e9bfe1f65920957&ipo=images",
-                    fit: BoxFit.cover,
-                  )
-                ),
-              ),
-              const SliverToBoxAdapter(
-                child: Padding(
-                  padding: EdgeInsets.symmetric(horizontal: 14, vertical: 8),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      TextWidget("Salão Mallonzete"),
-                      TextWidget(
-                        "Av Plinio Valério",
-                        customFontsize: 13,
-                      ),
-                    ],
-                  ),
-                ),
-              ),
-              SliverPersistentHeader(
-                delegate: _SliverAppBarDelegate(
-                  
-                  const TabBar(
-                    labelColor: Colors.black87,
-                    labelStyle: TextStyle(
-                      fontFamily: "Sora"
+    String urlImage = "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.5323gYZjoqFwr0zrN28KPAHaE8%26pid%3DApi&f=1&ipt=96d6cb3c800087f979dcfed334aaa919f198619cfcf66cec8e9bfe1f65920957&ipo=images";
+
+    return DefaultTabController(
+      length: 3,
+      child: Scaffold(
+        body: SafeArea(
+          child: Column(
+            children: [
+              Stack(
+                children: [
+                  Image.network(urlImage),
+                  Container(
+                    width: MediaQuery.of(context).size.width,
+                    decoration: BoxDecoration(
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.black.withOpacity(0.4),
+                          spreadRadius: 10,
+                          blurRadius: 20,
+                          offset: const Offset(0, 3)
+                        ),
+                      ],
                     ),
-                    unselectedLabelStyle: TextStyle(
-                      fontFamily: "Sora"
+                    alignment: Alignment.topLeft,
+                    child: IconButton(
+                      onPressed: () => context.pop(),
+                      icon: const Icon(
+                        Icons.chevron_left_rounded,
+                        size: 42,
+                        color: Colors.white,
+                      )
                     ),
-                    unselectedLabelColor: Colors.grey,
-                    tabs: [
-                      Tab(text: "Serviços"),
-                      Tab(text: "Portfólio"),
-                      Tab(text: "Informações"),
-                    ],
                   ),
-                ),
-                pinned: true,
+                ],
               ),
-            ];
-          },
-          body: const Padding(
-            padding:  EdgeInsets.only(left: 16, right: 16),
-            child: TabBarView(
-              children: [
-                ServicesOfferPage(),
-                Portfolio(),
-                Informations()
-              ]
+            const TabBar(
+              labelColor: Colors.black87,
+              labelStyle: TextStyle(fontFamily: "Sora"),
+              unselectedLabelStyle: TextStyle(fontFamily: "Sora"),
+              unselectedLabelColor: Colors.grey,
+              tabs: [
+                Tab(text: "Serviços"),
+                Tab(text: "Portfólio"),
+                Tab(text: "Informações"),
+              ],
             ),
-          )
-        ),
-      ),
+            const Expanded(
+              child: TabBarView(
+                children: [
+                  ServicesOfferPage(),
+                  Portfolio(),
+                  Informations()
+                ]
+              ),
+            ),
+          ],
+                ),
+        )),
     );
-  }
-}
-
-class _SliverAppBarDelegate extends SliverPersistentHeaderDelegate {
-  _SliverAppBarDelegate(this._tabBar);
-
-  final TabBar _tabBar;
-
-  @override
-  double get minExtent => _tabBar.preferredSize.height;
-  @override
-  double get maxExtent => _tabBar.preferredSize.height;
-
-  @override
-  Widget build(BuildContext context, double shrinkOffset, bool overlapsContent) {
-    return  Container(
-      color: Colors.white,
-      child: _tabBar
-    );
-  }
-
-  @override
-  bool shouldRebuild(_SliverAppBarDelegate oldDelegate) {
-    return false;
   }
 }
