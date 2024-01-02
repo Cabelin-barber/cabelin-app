3659486cbbc97adf6c56547056c8af81003c9631
iCarlosAugusto <augusto.dev@proton.me>
2024-01-02 00:08:57 -0300

    feat: :sparkles: professeional model

---
+++ lib/models/professional_model.dart
@@ -0,0 +1,21 @@
+import 'package:cabelin_v2/models/picture_model.dart';
+import 'package:json_annotation/json_annotation.dart';
+
+part 'professional_model.g.dart';
+
+@JsonSerializable()
+class ProfessionalModel {
+
+  String id;
+  String name;
+  PictureModel profilePicture;
+
+  ProfessionalModel({
+    required this.id,
+    required this.name,
+    required this.profilePicture
+  });
+
+  factory ProfessionalModel.fromJson(Map<String, dynamic> json) => _$ProfessionalModelFromJson(json);
+  Map<String, dynamic> toJson() => _$ProfessionalModelToJson(this);
+}
---
+++ lib/models/professional_model.g.dart
@@ -0,0 +1,22 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'professional_model.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+ProfessionalModel _$ProfessionalModelFromJson(Map<String, dynamic> json) =>
+    ProfessionalModel(
+      id: json['id'] as String,
+      name: json['name'] as String,
+      profilePicture:
+          PictureModel.fromJson(json['profilePicture'] as Map<String, dynamic>),
+    );
+
+Map<String, dynamic> _$ProfessionalModelToJson(ProfessionalModel instance) =>
+    <String, dynamic>{
+      'id': instance.id,
+      'name': instance.name,
+      'profilePicture': instance.profilePicture,
+    };
