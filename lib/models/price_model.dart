c9e4abb1771089618505fc40ae0770ff76cf4bd8
iCarlosAugusto <augusto.dev@proton.me>
2024-01-02 00:08:45 -0300

    feat: :sparkles: price model

---
+++ lib/models/price_model.dart
@@ -0,0 +1,19 @@
+import 'package:json_annotation/json_annotation.dart';
+
+part 'price_model.g.dart';
+
+@JsonSerializable()
+class PriceModel {
+
+  String currency = "brl";
+  int value;
+
+  PriceModel({
+    this.currency = "brl",
+    required this.value,
+
+  });
+
+  factory PriceModel.fromJson(Map<String, dynamic> json) => _$PriceModelFromJson(json);
+  Map<String, dynamic> toJson() => _$PriceModelToJson(this);
+}
---
+++ lib/models/price_model.g.dart
@@ -0,0 +1,18 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'price_model.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+PriceModel _$PriceModelFromJson(Map<String, dynamic> json) => PriceModel(
+      currency: json['currency'] as String? ?? "brl",
+      value: json['value'] as int,
+    );
+
+Map<String, dynamic> _$PriceModelToJson(PriceModel instance) =>
+    <String, dynamic>{
+      'currency': instance.currency,
+      'value': instance.value,
+    };
