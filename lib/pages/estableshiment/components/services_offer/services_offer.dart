78585e9fbc45b9d2337d072654d87cd8fed3b368
iCarlosAugusto <augusto.dev@proton.me>
2024-01-02 00:09:51 -0300

    refactor: :recycle: Refactor

--- lib/pages/estableshiment/components/services_offer/services_offer.dart
+++ lib/pages/estableshiment/components/services_offer/services_offer.dart
@@ -1,32 +1,73 @@
+import 'package:cabelin_v2/pages/estableshiment/components/services_offer/service_offer_controller.dart';
 import 'package:cabelin_v2/widgets/button_widget.dart';
+import 'package:cabelin_v2/widgets/list_refresh_widget.dart';
 import 'package:cabelin_v2/widgets/text_widget.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_mobx/flutter_mobx.dart';
 import 'package:go_router/go_router.dart';
+import 'package:lottie/lottie.dart';
 
-class ServicesOfferPage extends StatelessWidget {
-  const ServicesOfferPage({super.key});
+class ServicesOfferPage extends StatefulWidget {
+
+  ServicesOfferPage({
+    super.key,
+    required this.establishmentId
+  });
+
+  String establishmentId;
+
+  @override
+  State<ServicesOfferPage> createState() => _ServicesOfferPageState();
+}
+
+class _ServicesOfferPageState extends State<ServicesOfferPage> with AutomaticKeepAliveClientMixin{
+  
+
+  @override
+  bool get wantKeepAlive => true;
 
   @override
   Widget build(BuildContext context) {
-    return Container(
+    super.build(context);
+
+    ServiceOfferController serviceOfferController = ServiceOfferController(establishmentId: widget.establishmentId);
+
+    return Padding(
       padding: const EdgeInsets.all(16),
-      child: ListView.separated(
-      separatorBuilder: (_, __) => const Divider(),
-      itemCount: 2,
-      padding: EdgeInsets.zero,
-      itemBuilder: (_, __) => ListTile(
-        leading: const TextWidget(
-          "R\$ 35",
+      child: Observer(
+        builder: (context) =>   ListRefreshWidget(
+        isLoading: serviceOfferController.isLoading,
+        onRefresh: () async {
+          serviceOfferController.getServices();
+        },
+        customEmpty: Center(
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              LottieBuilder.asset(
+                "assets/empty.json",
+              ),
+              const TextWidget(
+                "Sem fotos por enquanto",
+              ),
+            ],
+          ),
+        ),
+        separatorBuilder: (_, __) => const Divider(),
+        itemCount: 2,
+        itemBuilder: (_, index) => ListTile(
+        leading: TextWidget(
+          "R\$ ${serviceOfferController.services[index].price.value.toString()}",
           customWeight: FontWeight.w800,
           customFontsize: 16,
         ),
-        title: const TextWidget(
-          "Corte degradê",
+        title: TextWidget(
+          serviceOfferController.services[index].name,
           customWeight: FontWeight.w800,
           customFontsize: 16,
         ),
-        subtitle: const TextWidget(
-          "Na Regua",
+        subtitle: TextWidget(
+          serviceOfferController.services[index].description,
           customFontsize: 13,
         ),
         trailing: ButtonWidget(
@@ -36,6 +77,7 @@ class ServicesOfferPage extends StatelessWidget {
           }
         ),
       ),
-    ));
+     )) 
+    );
   }
 }
