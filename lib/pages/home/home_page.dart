dd8c8e2dc153629178ea5ff58518450b72206b22
iCarlosAugusto <augusto.dev@proton.me>
2024-01-14 16:45:24 -0300

    feat: :sparkles: home iniital

--- lib/pages/home/home_page.dart
+++ lib/pages/home/home_page.dart
@@ -1,3 +1,4 @@
+import 'package:cabelin_v2/widgets/button_widget.dart';
 import 'package:cabelin_v2/widgets/text_widget.dart';
 import 'package:flutter/material.dart';
 
@@ -6,6 +7,23 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return TextWidget("Home");
+    return SafeArea(
+      child: SingleChildScrollView(
+        child: Column(
+          children: [
+            Container(
+              width: 300,
+              height: 300,
+              color: Colors.blue,
+            ),
+            const TextWidget("Parece que você ainda não fez \n nenhum agendamento"),
+            ButtonWidget(
+              title: "Agendar agora",
+              onTap: () {}
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
