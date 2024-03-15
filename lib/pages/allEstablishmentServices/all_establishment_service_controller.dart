955d08811ed09d25893a078e27c467e1b2f0d50e
iCarlosAugusto <augusto.dev@proton.me>
2024-03-14 21:02:34 -0300

    feat:

--- lib/pages/allEstablishmentServices/all_establishment_service_controller.dart
+++ lib/pages/allEstablishmentServices/all_establishment_service_controller.dart
@@ -1,37 +1,33 @@
 import 'package:cabelin_v2/models/service_model.dart';
 import 'package:cabelin_v2/utils/apiRequest.dart';
-import 'package:dio/dio.dart';
-import 'package:mobx/mobx.dart';
-part 'all_establishment_service_controller.g.dart';
+import 'package:get/get_state_manager/src/simple/get_controllers.dart';
 
-class AllEstablishmentServiceController = _AllEstablishmentServiceControllerBase with _$AllEstablishmentServiceController;
-
-abstract class _AllEstablishmentServiceControllerBase with Store {
+class AllEstablishmentServiceController extends GetxController {
   final api = Api.dio;
   String establishmentId;
 
-  ObservableList<ServiceModel> services = ObservableList<ServiceModel>();
-
-  @observable
+  List<ServiceModel> services = [];
   bool isLoading = false;
 
-  _AllEstablishmentServiceControllerBase({required this.establishmentId}) {
+  AllEstablishmentServiceController({required this.establishmentId}) {
     getAllServices();
   }
   
-  @action
   Future<void> getAllServices() async {
     try {
       services.clear();
       isLoading = true;
-      Response response = await api.get("/establishments/$establishmentId/beauty-services?page=0&size=10");
+      var response = await api.get("/establishments/$establishmentId/beauty-services?page=0&size=10");
       services.addAll(List.from(response.data['content'].map((model) => ServiceModel.fromJson(model))));
       isLoading = false;
+      update();
     } catch (e) {
+      print(e);
       //[TO-DO implementar Catch]
       print("ERRO!");
     } finally{
       isLoading = false;
+      update();
     }    
   }
 }
