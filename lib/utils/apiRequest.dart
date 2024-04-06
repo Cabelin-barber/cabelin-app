47fd66059c1176a266a608a27757f6c6e44f135d
iCarlosAugusto <augusto.dev@proton.me>
2024-04-05 21:07:11 -0300

    feat: :sparkles: trocando o endpoin

--- lib/utils/apiRequest.dart
+++ lib/utils/apiRequest.dart
@@ -4,7 +4,7 @@ class Api {
 
   static final dio = Dio(
     BaseOptions(
-      baseUrl: "http://ec2-3-129-45-22.us-east-2.compute.amazonaws.com:8080/api-cabelin/v1",
+      baseUrl: "http://ec2-18-227-49-126.us-east-2.compute.amazonaws.com:8080/api-cabelin/v1",
     )
   );
 }
